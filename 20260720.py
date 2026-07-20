fa776c064ba552f816416a6f4074d853556edf16
kang123554 <opensocks@naver.com>
2026-07-20 19:53:39 +0900

    Add files via upload

---
+++ 20260720.py
@@ -0,0 +1,93 @@
+
+##print("# 연습문제")
+##print("\\\\\\")
+##print("-" * 8)
+
+##print("안녕하세요"[1:3])
+##print("안녕하세요"[2:4])
+##print("안녕하세요"[1:])
+##print("안녕하세요"[:3])
+
+##name = input("닉네임을 입력하세요 : ")
+##print(f"환영합니다 {name}님!")
+##
+##num1 = float(input("실수를 입력하세요 : "))
+##print(f"{(num1*10):.3f}")
+##
+##num1 = int(input("첫번째 숫자를 입력하세요 : "))
+##num2 = int(input("두번째 숫자를 입력하세요 : "))
+##num3 = int(input("세번째 숫자를 입력하세요 : "))
+##print(f"총합은 {num1+num2+num3}")
+##
+##r = int(input("반지름을 입력하세요 : "))
+##print(f"원의 넓이는 {r*r*3.14:.1f}")
+##print(f"원의 둘레길이는 {r*2*3.14:.1f}")
+##
+##num1 = int(input("윗변을 입력하세요 : "))           
+##num2 = int(input("밑변을 입력하세요 : "))
+##num3 = int(input("높이를 입력하세요 : "))
+##print(f"사다리꼴의 넓이는 {(num1+num2)*num3/2:.1f}")
+      
+##str1 = 'Hello string!'
+##str2 = "안녕 문자열!"
+##str3 = """안녕 변수!
+##"""
+##str1 = "파이썬"
+##str2 = "참 쉽다"
+##str3 = str1 +"," + str2 + "!!"
+##print(str3)
+
+##str1 = "파이썬 Lvup"
+##str2 = str1 * 3
+##print(str2)
+
+##nut1 = '밤양갱'
+##nut2 = "달디단 밤양갱"
+##nut3 = """달디달고 달디달고,
+
+##sweat = '달디'
+##sweat2 = sweat * 10
+##number = 255
+##message = sweat2 + ' ' + '단 밤양갱' + str(number) + "개"
+##print(message)
+
+##line = "="* 20
+##header = "밤양갱 대박 세일"
+##footer = "놓치지 마세요!"
+##message = line + "\n" + header + "\n" + line + "\n" + footer + "\n" + line
+##print(message)
+
+##str1 = input("아이디 : ")
+##str2 = input("비밀번호 : ")
+##message = "당신의 아이디는" + "str1\" + "이며," + ' ' + "비밀번호는" + "str2\" + "입니다."
+##print(message)
+
+##str1 = "파이썬 문자열을 골라보자"
+##print(str1[-2])
+
+##str2 = "이샘코딩학원"
+####print(str2[1:3])
+##print(str2[::-3])
+##print(str2[::3])
+
+##word = "문자열과 인덱스"
+##print(word[0])
+##print(word[3])
+##print(word[5])
+##print(word[-1])
+
+word = "슬라이싱으로 다양하게 문자를 잘라봅시다"
+print(word[0:4])
+print(word[7:9])
+print(word[5:])
+print(word[:12])
+print(word[::3])
+print(word[::-3])
+
+
+
+
+
+
+
+
