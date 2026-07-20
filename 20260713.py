941c1a134286a04e1a4678af2573b4a80848edb5
kang123554 <opensocks@naver.com>
2026-07-20 18:09:47 +0900

    Add files via upload

---
+++ 20260713.py
@@ -0,0 +1,133 @@
+##print("# 하나만 출력합니다.")
+##print("hello Python Programming...!")
+##print()
+##
+##print("# 여러 개를 출력합니다.")
+##print(10, 20, 30, 40, 50)
+##print("안녕하세요", "저의", "이름은","윤인성입니다!")
+##
+##
+##print("Hellow World")
+##print("이샘", "코딩", "전문", "학원")
+##print("코딩세계에", "오신걸", "환영합니다.")
+
+      
+#print("이샘", "학원",'123','yyy',sep="..")
+##print("이샘코딩학원", '456456',end="")
+##
+##print("안녕하세요",end="")
+##print("안녕하세요")
+##
+##print("안녕하1000\\세요\n")
+##print("안녕하세요\n")
+##
+##print('안녕"하세요',end="")
+##print("안녕'하세요",end='')
+##
+##print("\",\'는 따옴표의 대체 기회입니다.")
+##print("\\n은 줄바꿈의 대체 기호입니다.")
+
+##print("\",\'는 따옴표의 대체 기호입니다.")
+##print("wwn은 줄바꿈의 대체 기호입니다.")
+
+##print("안녕하세요wwn")
+##print("안녕하세요wwn")
+
+##print("안녕하세요WWn")
+##print("안녕하세요WWn")
+
+##print("안녕하세요Wn")
+##print("안녕하세요Wn")
+
+##print("따옴표는 '역할'이 같아요")
+##print('따옴표는 "역할"이 같아요')
+
+##print("안녕하세요Wn")
+##print("안녕하세요Wn")
+
+##apple = "사과"
+##print(apple, 1, "상자")
+##print(apple,  1,  "상자")
+
+##apple = "사과"
+##print(apple, 1, "상자", sep=" ++")
+
+##print("이샘")
+##print("코딩")
+##print("학원")
+
+##print("이샘")
+##print("코딩")
+##print("학원",end="!")
+
+##print("이샘",end="")
+##print("코딩",end="")
+##print("학원",end="!")
+
+##number = 10
+##pi = 3.141595
+##name = "이샘"
+##check = true
+
+##num1 = 10
+##print(num1, type(num1))
+
+##num2 = 3.14
+##print(num2, type(num2))
+
+##m_str = "안녕하세요"
+##print(m_str, type(m_str))
+
+##print(1+1, type(1+1))
+
+##print(0.3+2.04, type(0.3+2.04))
+
+##print(0.3+2.04)
+
+##print("이샘", "학원",sep="코딩전문")
+
+##print("\
+
+##print(1,2,3,sep="+",end="=6")
+
+##print('"이샘코딩전문학원"')
+
+##print(f">{'abc':5s}<")
+##print(f">{3.141595:6.2f}<")
+
+##print("48 > %d #o" % (48,48),end="")
+##print("48 > %x %c" % (48,48))
+##print("%c %c" % (65,0x41))
+##print("%c %c" % (66,0x52))
+##print("%f %e" % (0.132, 0.132))
+##print("이샘%s학원" % ("코딩"))
+
+##num1=17
+##print(num1)
+##print("10진수 17 = %d"%(num1))
+##print("10진수 17의 8진수 = %o"%(num1))
+##print("10진수 17의 16진수 = %x"%(num1))
+      
+##num2 = 97
+##print("%d %c" % (num2,num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 20260715.py
@@ -0,0 +1,93 @@
+##num1=17
+##print(num1)
+##print("10진수 17 = %d"%(num1))
+##print("10진수 17의 8진수 = %o"%(num1))
+##print("10진수 17의 16진수 = %x"%(num1))
+
+##num2=97
+##print("%d %c" % (num2, num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본출력 %f" % (float1))
+##print("지수표기법 %e" % (float1))
+##print("소수 둘째자리까지 %.2f" % (float1))
+
+##txt1="hello"
+##txt2="world"
+##print("기본출력\n%s %s" % (txt1, txt2))
+##print("7칸 출력\n%7s %7s" % (txt1, txt2))
+
+##print("48 > {0} {1:o}".format(48,48))
+##print("48 > {0:x} {1:c}".format(48,48))
+##print("{0:c} {1:c}".format(65,0x41))
+
+##number=48
+##hexA=0x41
+##coding="코딩"
+##
+##print(f"48 > {number} {number:o}")
+##print(f"48 > {number} {number:c}")
+##print(f"{hexA} {hexA:c}")
+##print(f"{hexA} {hexA:c}")
+##print(f"{0.132} {0.132:e}")
+##print(f"이샘{coging}학원")
+
+##num1=17
+##print(num1)
+##print(f'10진수 17 = {num1}')
+     
+##print(f'10진수 17의 8진수 = {num1:o}')
+##print(f'10진수 17의 16진수 = {num1:x}')
+
+##float1=1234.567
+##print(float1)
+##print(f"기본 출력 {float1:f}")
+##print(f"지수표기법 {float1:e}")
+##print(f"소수 둘째자리까지 {float1:.2f}")
+
+##txt1="hello"
+##txt2="world!"
+##print(f'기본출력\n{txt1:s} {txt2:s}')
+##print(f'7칸출력wn{txt1:7s} {txt2:7s}?')
+      
+##r = 5
+##pi = 3.14
+##print('넓이는 %d * %d * %.2f = %.1f'% (r, r,  pi, r*r*pi))
+##print('넓이는 {0} * {0} * {1} = {2}'.format(r, pi, r*r*pi))
+##print(f'넓이는 {r} * {r} * {pi} = {r*r*pi}')
+
+##golden_ratio = 1.61804
+##print(f'소수 첫째 자리까지 출력 : {golden_ratio:.1f}')
+##print(f'소수 둘째 자리까지 출력 : {golden_ratio:.2f}')
+##print(f'소수 셋째 자리까지 출력 : {golden_ratio:.3f}')
+##print(f'소수 넷째 자리까지 출력 : {golden_ratio:.4f}')
+
+##egg = input("계란은 몇 개? 8")
+##print(egg, type(egg))
+
+##egg = input("계란은 몇 개? ")
+##print(egg, type(egg))
+##egg = int(egg)
+##print(egg, type(egg))
+
+##attack = input("공격력은?")
+##scale = input("배율은?")
+##
+##print("데미지는 ", attack*scale)
+
+##attack = int(input("공격력은?"))
+##scale = float(input("배율은?"))
+##print("데미지는", attack*scale)
+
+##minute = input("분을 입력하세요 :")
+##minute = int(minute)
+##second = 60*minute
+
+##text = str(minute)+"분="+str(second)+"초"
+##print(text)
+
+name = input("닉네임을 입력하세요 : ")
+print(f"환영합니다 {name}!")
+
+     
