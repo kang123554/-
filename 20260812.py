50ee34fc1d64fa5649c31cb1d2729bd58172f7b9
kang123554 <opensocks@naver.com>
2026-08-12 19:52:02 +0900

    Add files via upload

---
+++ 20260812.py
@@ -0,0 +1,97 @@
+##num = int(input("다섯 자리 숫자를 입력합니다 : "))
+##
+##num1 = num % 10        
+##num2 = num // 10       
+##num10 = num2 % 10      
+##num3 = num2 // 10
+##num100 = num3 % 10  
+##num4 = num3 // 10
+##num1000 = num4 % 10  
+##num5 = num4 // 10
+##num10000 = num5 % 10  
+##num =num % 10
+##
+##print(f"만의 자리 : {num10000}")
+##print(f"천의 자리 : {num1000}")
+##print(f"백의 자리 : {num100}")
+##print(f"십의 자리 : {num10}")
+##print(f"일의 자리 : {num1}")
+
+##num1 = int(input("첫 번째 숫자 입력 : "))
+##num2 = int(input("두 번째 숫자 입력 : "))
+##num3 = int(input("세 번째 숫자 입력 : "))
+##
+##print(f"10000 += num1 -> {10000 + num1}")
+##print(f"10000 -= num2 -> {10000 - num2}")
+##print(f"10000 *= num3 -> {10000 * num3}")
+
+##txt = input("문자열 입력 : ")
+##num = input("숫자 입력 : ")
+##
+##txt2 = txt
+##txt += num
+##txt2 *= int(num)
+##
+##print(txt)
+##print(txt2)
+
+
+##str1, str2, str3 = input(f"숫자 3개를 입력해주세요 : ").split()
+##num = 100 
+##num1 = int(str1)#1
+##num2 = int(str2)#2
+##num3 = int(str3)#3
+##
+##print(f"숫자 3개를 입력해주세요 : ", end="")
+##num = 100 
+##num1 = int(input())#1
+##num2 = int(input())#2
+##num3 = int(input())#3
+##print(f"  {num1 - num2 - num3}")
+##
+##num -= num1 
+##num -= num2 
+##num -= num3 
+##print(num)
+
+##num1 = input("숫자를 입력해주세요 : ")
+##txt1 = str("삐약")
+##
+##txt2 = txt1
+##txt1 += num1 
+##txt2 *= int(num1)
+##
+##print(txt2)
+
+##num1 = input("")
+##print(num1*9)
+##
+##num1 = int(input("숫자를 입력해주세요 : "))
+##print(f"삐약 *= {num1}")
+
+##egg = int(input("계란 개수 입력 : "))
+##tmp = egg
+##tmp %= 30
+##egg //= 30
+##
+##print("계란 {0}판, 나머지 {1}개".format(egg, tmp))
+
+##first = int(input("피자를 몇 등분할까? : "))
+##second = int(input("한 조각을 몇 등분할까? : "))
+##pizza = 100
+##pizza /= first
+##pizza /= second
+##
+##print(f"이 조각은 피자 한 판의 {pizza:.2f}%")
+
+num = 1357
+num1 %
+
+
+print(
+
+
+
+
+
+
