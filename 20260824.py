5d3719d91e7b03dc0f713e23160592d380487172
kang123554 <opensocks@naver.com>
2026-08-24 19:52:47 +0900

    Add files via upload

---
+++ 20260824.py
@@ -0,0 +1,136 @@
+##num = int(input("숫자를 입력해주세요 : "))
+##if num % 2 == 0:
+##    print("14는 짝수입니다.")
+
+##num = int(input("현재 온도를 입력해주세요 : "))
+##if num < 25:
+##    print("날씨 상태: 시원한  날씨")
+##if 25 <= num < 30:
+##    print("날씨 상태: 따뜻한 날씨")
+##if num >= 30:
+##    print("날씨 상태: 더운 날씨")
+
+
+##txt = input("문자열을 입력하세요: ")
+##if ' ' in txt:
+##    print("공백이 포함되어 있습니다.")
+##if ' ' not in txt:
+##    print("공백이 포함되어 있지 않습니다.")
+
+##age = int(input("나이를 입력해주세요 : "))
+##if age >= 0:
+##    print("전체관람가")
+##if age >= 12:
+##    print("12세 관람가")
+##if age >= 15:
+##    print("15세 관람가")
+    
+##number = 3
+##if number == 15:
+##    print("이 숫자는 15입니다.")
+##else:
+##    print("이 숫자는 15가 아닙니다.")
+
+##number = 3
+##if number == 15:
+##    print("이 숫자는 15입니다.")
+##elif number == 3:
+##    print("이 숫자는 3입니다.")
+##else:
+##    print("이 숫자는 3도 15도 아닙니다.")
+    
+##string = input("문자를 입력하세요: ")
+##if string.isalpha():
+##    print("알파벳 혹은 한글입니다.")
+##elif string.isnumeric():
+##    print("숫자입니다.")
+##else:
+##    print("특수 문자입니다.")
+
+##height = int(input("키를 입력해주세요: "))
+##if height > 170:
+##    print("키가 170 이상입니다.")
+##elif height > 160:
+##    print("키가 160 이상입니다.")
+##elif height > 150:
+##    print("키가 150 이상입니다.")
+##else:
+##    print("키가 150 미만입니다.")
+
+##height = int(input("키를 입력해주세요: "))
+##if height > 150:
+##    print("키가 150 이상입니다.")
+##elif height > 160:
+##    print("키가 160 이상입니다.")
+##else:
+##    print("키가 150 미만입니다.")
+
+##txt = input("방향키를 입력해주세요 (w/a/s/d) : ")
+##if "w" in txt:
+##    print("위 방향키를 입력하셨습니다.")
+##elif "a" in txt:
+##    print("좌 방향키를 입력하셨습니다.")
+##elif "s" in txt:
+##    print("아래 방향키를 입력하셨습니다.")
+##elif "d" in txt:
+##    print("우 방향키를 입력하셨습니다.")
+##else:
+##    print("잘못 입력하셨습니다.")
+
+##num = int(input("숫자를 입력해주세요 : "))
+##if num == 0: 
+##    print("number은 0입니다.")
+##if num % 2 == 0:
+##    print("number은 짝수입니다.")
+##if num > 0: 
+##    print("number은 양수입니다.")
+##if num % 2 == 1:
+##    print("number은 홀수입니다.")
+##if num < 0:
+##    print("number은 음수입니다.")
+##if num % 2 == 0:
+##    print("number은 짝수입니다.")
+
+##num = int(input("숫자를 입력해주세요 : "))
+##
+##if num == 0: 
+##    print("number은 0입니다.")
+##elif num > 0: 
+##    print("number은 양수입니다.")
+##else:
+##    print("number은 음수입니다.")
+##
+##    
+##if num % 2 == 0:
+##    print("number은 짝수입니다.")
+##else:
+##    print("number은 홀수입니다.")
+##
+##
+##num = int(input("숫자를 입력해주세요 : "))
+##if 10 <= num <= 99:
+##    print("두자리 숫자입니다.")
+##else:
+##    print("두자리 숫자가 아닙니다.")
+
+##num = int(input("숫자를 입력해주세요 : "))
+##if num % 3 == 0:
+##    print("이 숫자는 3의 배수입니다.")
+##    if num % 6 == 0:
+##        print("이 숫자는 6의 배수입니다.")
+##    if num % 9 == 0:
+##        print("이 숫자는 9의 배수입니다.")
+
+##txt = input("알파벳을 입력해주세요 : ")
+##if txt.islower():
+##    print("소문자 입니다.")
+##elif txt.isupper():
+##    print("대문자 입니다.")
+
+
+
+
+
+
+
+          
