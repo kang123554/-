77d38064ffc4a441f53eded17a8d60e129379275
kang123554 <opensocks@naver.com>
2026-08-25 20:39:16 +0900

    Add files via upload

---
+++ 20020825.py
@@ -0,0 +1,116 @@
+##number = input("정수 이력> ")
+##number = int(number)
+##
+##if number % 2 == 0:
+##    print("짝수입니다")
+##if number % 2 == 1:
+##    print("홀수입니다")
+
+##number = input("정수 이력> ")
+##number = int(number)
+##
+##if number % 2 == 0:
+##    print("짝수입니다")
+##else:
+##    print("홀수입니다")
+
+##import datetime
+##now = datetime.datetime.now()
+##month = now.month
+##
+##if 3 <= month <= 5:
+##    print("현재는 봄입니다")
+##elif 6 <= month <= 8:
+##    print("현재는 여름입니다")
+##elif 9 <= month <= 11:
+##    print("현재는 가을입니다")
+##else:
+##    print("현재는 겨울입니다")
+
+##score = float(input("학점 입력> "))
+##
+##if score == 4.5:
+##    print("신")
+##elif 4.2 <= score < 4.5:
+##    print("교수님의 사람")
+##elif 3.5 <= score < 4.2:
+##    print("현 체제의 수호자")
+##elif 2.8 <= score < 3.5:
+##    print("일반인")
+##elif 2.3 <= score < 3.2:
+##    print("일탈을 꿈꾸는 소시민")
+##elif 1.75 <= score < 2.3:
+##    print("오락문화의 선구자")
+##elif 1.0 <= score < 1.75:
+##    print("불가촉천민")
+##elif 0.5 <= score < 1.0:
+##    print("자벨레")
+##elif 0 <= score < 0.5:
+##    print("플랑크톤")
+##elif score == 0:
+##    print("시대를 앞서가는 혁명의 씨앗")
+
+##score = float(input("학점 입력> "))
+##
+##if score == 4.5:
+##    print("신")
+##elif 4.2 <= score:
+##    print("교수님의 사람")
+##elif 3.5 <= score:
+##    print("현 체제의 수호자")
+##elif 2.8 <= score:
+##    print("일반인")
+##elif 2.3 <= score:
+##    print("일탈을 꿈꾸는 소시민")
+##elif 1.75 <= score:
+##    print("오락문화의 선구자")
+##elif 1.0 <= score:
+##    print("불가촉천민")
+##elif 0.5 <= score:
+##    print("자벨레")
+##elif 0 <= score:
+##    print("플랑크톤")
+##else:
+##    print("시대를 앞서가는 혁명의 씨앗")
+
+##num = int(input("숫자를 입력해주세요 : "))
+##if num % 2 == 0:
+##    print("14는 짝수입니다.")
+##num = int(input("숫자를 입력해주세요 : "))
+##if num % 2 == 0:
+##    print("14는 짝수입니다.")
+
+##txt = input("문자열을 입력하세요: ")
+##if ' ' in txt:
+##    print("공백이 포함되어 있습니다")
+##txt = input("문자열을 입력하세요: ")
+##if ' ' not in txt:
+##    print("공백이 포함되어 있지 않습니다")
+
+##if 0:
+##    print("0은 true로 변환됩니다.")
+##else:
+##    print("0은 false로 변환됩니다.")
+##    print()
+##
+##if "":
+##    print("빈 문자열은 True로 변환됩니다.")
+##else:
+##    print("빈 문자열은 False로 변환됩니다.")
+
+##if zreo == 0
+##else:
+
+##number = input("정수 입력> ")
+##number = int(number)
+##
+##if number > 0:
+##else:
+
+##number = input("정수 입력> ")
+##number = int(number)
+##
+##if number > 0:
+##    pass
+##else:
+##    pass
