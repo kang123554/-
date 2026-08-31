b7fac621ac4b5619aa19046e807a9492858a7496
kang123554 <opensocks@naver.com>
2026-08-31 18:38:20 +0900

    Add files via upload

---
+++ 20260831.py
@@ -0,0 +1,73 @@
+##count = 0
+##while count < 3:
+##    print("{0}번째의 while문!".format(count))
+##    count += 1
+
+##count = 0
+##while count < 3:
+##    print("{0}번째의 while문!".format(count))
+##count += 1
+
+##count = 0
+##while count < 3:
+##    print("{0}회 반복 중".format(count))
+##    count += 1
+
+##count = 3
+##while count > 0:
+##    print("{0}.....".format(count))
+##    count -= 1
+
+##number = 10
+##while number <= 1000000:
+##    print(f"{number}")
+##    number *= 10 
+
+##count = 0
+##while count < 100:
+##    if count % 3 == 0:
+##        print("3의 배수 : {0}".format(count))
+##    count += 1 
+
+##count = 1
+##while count <= 5:
+##    print(f"파이썬")
+##    count += 1
+
+##num = 1
+##count = 0 
+##
+##while num <= 10:
+##    if num = num + 1:
+##        print(num)
+##    num += 1
+
+##num = 1
+##while num <= 10:
+##    print(num + 1))
+##    num += 1 
+
+##num = 1
+##while num <= 200:
+##    if num % 5 == 0:
+##        print(num , end=" ")
+##    num += 1
+
+num = 1
+count = 0
+
+while num <= 200:
+    if num % 5 == 0:
+        print(num , end=" ")
+        count += 1
+    if count % 5 == 0:
+        print()
+
+
+    
+
+
+
+
+
+    
